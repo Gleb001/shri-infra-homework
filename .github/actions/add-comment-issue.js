9e9f203ae4e1b56135a347100fdb6872520729e4
gleb001 <lichagingleb@mail.ru>
2024-07-21 23:56:08 +0500

    update release flow

---
+++ .github/actions/add-comment-issue.js
@@ -0,0 +1,50 @@
+const github = require("@actions/github");
+const core = require("@actions/core");
+
+async function run() {
+    try {
+
+        const token = process.env.token;
+        const date = process.env.date;
+        const author = process.env.author;
+        const version = process.env.version;
+        const commits = process.env.commits;
+        const fix_number = process.env.fix_number;
+        const id_registry = process.env.id_registry;
+
+        const octokit = github.getOctokit(token);
+        const context = github.context;
+
+        const issue = await octokit.rest.issues.listForRepo({
+            owner: context.repo.owner,
+            repo: context.repo.repo,
+            state: 'open'
+        });
+
+        if (!issue) {
+            throw new Error(`Issue for release version ${version} not found`);
+        }
+
+        await octokit.rest.issues.createComment({
+            ...context.repo,
+            issue_number: issue.number,
+            body: (`
+                # Fix Release v${version}_fix${fix_number}
+
+                **Дата фикса:** ${date}
+                **Автор фикса:** ${author}
+                
+                **Список коммитов от предыдущего релизного тега:**
+                ${commits}
+                
+                **Ссылка на docker-образ:**
+                cr.yandex/${id_registry}/app:${version}_fix${fix_number}
+            `),
+        });
+
+    } catch (error) {
+        core.setFailed(`Action failed with error ${error}`);
+    }
+}
+
+run();
