575a58e3ff987aa1764d32cd6005466b088236a1
gleb001 <lichagingleb@mail.ru>
2024-07-21 11:34:28 +0500

    update new test

--- test/about.test.tsx
+++ test/about.test.tsx
@@ -6,6 +6,6 @@ describe('About', () => {
     it('Should render author name', () => {
         const { container } = render(<About />);
 
-        expect(container.textContent).toBe('AboutAuthor: Gleb" - not valid String"');
+        expect(container.textContent).toBe('AboutAuthor: Gleb"  - not valid String"');
     });
 });
