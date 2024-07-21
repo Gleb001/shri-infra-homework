9bf7b5933ac6b8b2dea801a1529498e12d6b8af0
gleb001 <lichagingleb@mail.ru>
2024-07-21 23:16:55 +0500

    update release flow

--- .github/actions/create-issue.js
+++ .github/actions/create-issue.js
@@ -14,43 +14,22 @@ async function run() {
         const octokit = github.getOctokit(token);
         const context = github.context;
 
-        console.log(octokit);
-
-        // await octokit.issues.create({
-        //     owner: context.repo.owner,
-        //     repo: context.repo.repo,
-        //     labels: [`release v${version}`],
-        //     title: `Release v${version}`,
-        //     body: `
-        //     # Release v${version}
-
-        //     **Дата:** ${date}
-        //     **Автор релиза:** ${author}
-        //     **Номер версии:** ${version}
-
-        //     **Список коммитов от предыдущего релизного тега:**
-        //     ${commits}
-
-        //     **Ссылка на docker-образ:**
-        //     cr.yandex/${id_registry}/app:${version}
-        //     `,
-        // });
         await octokit.rest.issues.create({
             ...context.repo,
             labels: [`release v${version}`],
             title: `Release v${version}`,
             body: (`
-            # Release ${{ version }}
+            # Release ${version}
 
-            **Дата:** ${{ date }}
-            **Автор релиза:** ${{ author }}
-            **Номер версии:** ${{ version }}
+            **Дата:** ${date}
+            **Автор релиза:** ${author}
+            **Номер версии:** ${version}
 
             **Список коммитов от предыдущего релизного тега:**
-            ${{ commits }}
+            ${commits}
 
             **Ссылка на docker-образ:**
-            cr.yandex/${{ id_registry }}/app:${{ version }}
+            cr.yandex/${id_registry}/app:${version}
         `),
         });
 
